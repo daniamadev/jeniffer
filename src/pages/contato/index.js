9cfd0ebc71acb4e21a9aae487197284e94bca207
Daniela Amadeu <danielapamadeu@gmail.com>
2020-04-03 17:00:17 -0300

    updating files

--- src/pages/contato/index.js
+++ src/pages/contato/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
+import axios from 'axios'
 
 export default class Contato extends Component {
+  state = {
+    conteudo: {}
+  }
+
+  componentDidMount = () => {
+    axios.get("http://localhost:3000/contato")
+      .then(resposta => {
+        this.setState({ conteudo: resposta.data })
+      })
+  }
+
   render() {
+    
+    const { titulo, subtitulo, informacao, contatos } = this.state.conteudo
+
     return (
       <>
         <Helmet>
@@ -12,14 +27,19 @@ export default class Contato extends Component {
           <div className="conteudo">
             <div className="flex">
               <div className="resumo">
-                <h1>Me contrate!</h1>
-                <p>Estou disponível para trabalhos freelancer</p>
-                <h2>Meus Contatos</h2>
-
-                <p>Email: meu@memail.com</p>
-                <p>Telefone: 19 0000-0000</p>
-                <p>Github</p>
-                <p>Linkedin</p>
+                <h1>{titulo}</h1>
+                <p>{subtitulo}</p>
+                <h2>{informacao}</h2>
+                {contatos !== undefined && (
+                  <ul>
+                  {contatos.map(item => (
+                    <li key={item.id}>{item.tipo}: {item.contato}</li>
+                  ))}
+                    <li>Github</li>
+                    <li>Linkedin</li>
+                  </ul>
+                )}
+                
               </div>
               <img src="http://trydo.rainbowit.net/assets/images/about/about-6.jpg" alt="Perfil" />
             </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,16 @@ import { Switch, Route } from 'react-router-dom'
 
 import Home from '../pages/home'
 import Contato from '../pages/contato'
+import Portfolio from '../pages/portfolio/index'
 
 function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/contato" exact component={Contato} />
+      <Route path="/portfolio" exact component={Portfolio} />
     </Switch>
   )
 }
 
-export default Routes
+export default Routes;
